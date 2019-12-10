79563d9b4530da7392ed3c62c1ae073b1d4fd564
cjb230 <james.barton@blueyonder.co.uk>
2019-12-10 12:30:04 +0100

    Works on all the tests but not the actual question!

--- day_10_a.py
+++ day_10_a.py
@@ -1,12 +1,19 @@
 SOURCE_FILE = 'day_10_input.txt'
 
+
+def gcd(a, b):
+    if a % b == 0:
+        return b
+    return gcd(b, a % b)
+
+
 def sign(x):
     if x > 0:
-        return 1.
+        return 1
     elif x < 0:
-        return -1.
+        return -1
     elif x == 0:
-        return 0.
+        return 0
     else:
         return x
 
@@ -31,13 +38,19 @@ def get_asteroid_map():
 
 
 def reduce_vector(x_in, y_in):
-    if abs(x_in) > abs(y_in):
+    x_result = x_in
+    y_result = y_in
 
-    elif abs(x_in) < abs(y_in):
-
+    if x_in != 0 and y_in != 0:
+        cd = gcd(abs(x_in), abs(y_in))
+        if cd > 1:
+            x_result = int(x_in / cd)
+            y_result = int(y_in / cd)
     else:
         x_result = sign(x_in)
         y_result = sign(y_in)
+
+    return x_result, y_result
 
 
 def main():
@@ -47,18 +60,25 @@ def main():
     x_size = len(space_map)
     y_size = len(space_map[0])
 
+    max_visible_asteroids = 0
     for location_x in range(x_size):
         for location_y in range(y_size):
             if space_map[location_x][location_y] == '#':
-                print('Processing asteroid at ' + str(location_x) + ', ' + str(location_y))
+                print('Processing asteroid at ' + str(location_x) + ', ' + str(location_y) + ' ...', end=' ')
+                asteroid_vector_set = set()
                 for considering_x in range(x_size):
                     for considering_y in range(y_size):
                         if space_map[considering_x][considering_y] == '#':
-                            print('  Considering asteroid at ' + str(considering_x) + ', ' + str(considering_y))
+                            #print('  Considering asteroid at ' + str(considering_x) + ', ' + str(considering_y))
                             vector_x = considering_x - location_x
                             vector_y = considering_y - location_y
-
-
+                            if max(abs(vector_x), abs(vector_y)) > 0:  # ignoring the asteroid we're on
+                                asteroid_vector_set.add(tuple(reduce_vector(vector_x, vector_y)))
+                currently_visible = len(asteroid_vector_set)
+                print(str(currently_visible) + ' visible')
+                if currently_visible > max_visible_asteroids:
+                    max_visible_asteroids = currently_visible
+    print('Max visible = ' + str(max_visible_asteroids))
 
 
 if __name__ == "__main__":
