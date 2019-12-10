6ccfe0a566873a3e540a1b87cf3b66069497112e
cjb230 <james.barton@blueyonder.co.uk>
2019-12-10 17:21:31 +0100

    Seems to work now...
    ...so probably a typo when I originally put the answer into the web form!

--- day_10_a.py
+++ day_10_a.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 SOURCE_FILE = 'day_10_input.txt'
 
 
@@ -61,11 +63,15 @@ def main():
     y_size = len(space_map[0])
 
     max_visible_asteroids = 0
+    asteroids_found = 0
     for location_x in range(x_size):
         for location_y in range(y_size):
             if space_map[location_x][location_y] == '#':
-                print('Processing asteroid at ' + str(location_x) + ', ' + str(location_y) + ' ...', end=' ')
-                asteroid_vector_set = set()
+                #print()
+                #print()
+                asteroids_found += 1
+                #print('Processing asteroid at ' + str(location_x) + ', ' + str(location_y) + ' ...', end=' ')
+                asteroid_vector_dict = defaultdict(int)
                 for considering_x in range(x_size):
                     for considering_y in range(y_size):
                         if space_map[considering_x][considering_y] == '#':
@@ -73,12 +79,26 @@ def main():
                             vector_x = considering_x - location_x
                             vector_y = considering_y - location_y
                             if max(abs(vector_x), abs(vector_y)) > 0:  # ignoring the asteroid we're on
-                                asteroid_vector_set.add(tuple(reduce_vector(vector_x, vector_y)))
-                currently_visible = len(asteroid_vector_set)
-                print(str(currently_visible) + ' visible')
+                                this_reduced_vector = tuple(reduce_vector(vector_x, vector_y))
+                                asteroid_vector_dict[this_reduced_vector] += 1
+                currently_visible = len(asteroid_vector_dict)
+                #print(str(currently_visible) + ' visible')
+                total_seen = sum(asteroid_vector_dict.values())
+                #print('Total seen = ' + str(total_seen))
+                blocking_dict = dict()
+                blocking_dict = {k:v for (k, v) in asteroid_vector_dict.items() if v > 1}
+                blocked = sum(blocking_dict.values()) - len(blocking_dict)
+                #print('Blocked = ' + str(blocked))
+                #print(str(blocking_dict))
+
                 if currently_visible > max_visible_asteroids:
                     max_visible_asteroids = currently_visible
-    print('Max visible = ' + str(max_visible_asteroids))
+                    best_location_x = location_x
+                    best_location_y = location_y
+
+    #print(str({k: v for k, v in asteroid_vector_dict if v > 1}))
+    print('Max visible = ' + str(max_visible_asteroids) + ', from ' + str(best_location_x) + ', ' + str(best_location_y))
+    #print('Asteroids found = ' + str(asteroids_found))
 
 
 if __name__ == "__main__":
